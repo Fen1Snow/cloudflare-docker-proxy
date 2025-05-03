d7d8166de13547597e1b1cbe78f6069db80d0648
fengxue <yh5280268@126.com>
2025-05-04 03:37:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -171,14 +171,3 @@ function responseUnauthorized(url) {
     headers: headers,
   });
 }
-import DOCS from './help.html'
- 
-// return docs
-if (url.pathname === "/") {
-  return new Response(DOCS, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
-  });
-}
